3caa6cce07acafcf0a5f4843e725b8713e9c44ea
kristis <kristisappdev@gmail.com>
2026-09-13 23:02:45 +0300

    Add the playGame() function
    Now it's calling playRound() and using score counters for a gameplay loop
    Which att the end of a set count of rounds prints the result of who won
    With the score counters.
    
    This is the final step implementation of rock paper scissors, no more
    needed

--- javascript.js
+++ javascript.js
@@ -15,62 +15,85 @@ function getHumanChoice() {
     return choice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    let verdict = "";
-    let beater = "";
-    let bested = "";
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+        let verdict = "";
+        let beater = "";
+        let bested = "";
 
-    if (humanChoice === computerChoice) {
-        verdict = "draw";
-    }
-    else if (humanChoice === "rock") {
-        if (computerChoice === "scissors") {
-            verdict = "win";
+        if (humanChoice === computerChoice) {
+            verdict = "draw";
+        }
+        else if (humanChoice === "rock") {
+            if (computerChoice === "scissors") {
+                verdict = "win";
+            }
+            else {
+                verdict = "lose";
+            }
+        }
+        else if (humanChoice === "paper") {
+            if (computerChoice === "rock") {
+                verdict = "win"
+            }
+            else {
+                verdict = "lose"
+            }
         }
         else {
-            verdict = "lose";
+            if (computerChoice === "rock") {
+                verdict = "lose"
+            }
+            else {
+                verdict = "win"
+            }
         }
-    }
-    else if (humanChoice === "paper") {
-        if (computerChoice === "rock") {
-            verdict = "win"
+
+        if (verdict === "win" || verdict === "draw") {
+            beater = humanChoice;
+            bested = computerChoice;
         }
         else {
-            verdict = "lose"
+            beater = computerChoice;
+            bested = humanChoice;
         }
+
+        // capitalize first letter:
+        beater = beater.charAt(0).toUpperCase() + beater.slice(1);
+        bested = bested.charAt(0).toUpperCase() + bested.slice(1);
+        const explanation = verdict === "draw" ? `${beater} draws ${bested}` :
+            `${beater} beats ${bested}`;
+
+        console.log(`You ${verdict}! ${explanation}`);
+        return verdict;
+    }
+
+    for (i = 0; i < 5; ++i) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        console.log(`Human chose ${humanChoice}, computer chose ${computerChoice}`);
+        const verdict = playRound(humanChoice, computerChoice);
+        if (verdict === "win") {
+            humanScore += 1;
+        }
+        else if (verdict === "lose") {
+            computerScore += 1;
+        }
+    }
+
+    if (humanScore > computerScore) {
+        console.log(`Congrats! You won with score ${humanScore} against ${computerScore}`);
+    }
+    else if (humanScore < computerScore) {
+        console.log(`Oof! You lost with score ${humanScore} against ${computerScore}`);
     }
     else {
-        if (computerChoice === "rock") {
-            verdict = "lose"
-        }
-        else {
-            verdict = "win"
-        }
+        console.log(`Perfect gameplay! You drew with score ${humanScore} against ${computerScore}`);
     }
-
-    if (verdict === "win" || verdict === "draw") {
-        beater = humanChoice;
-        bested = computerChoice;
-    }
-    else {
-        beater = computerChoice;
-        bested = humanChoice;
-    }
-
-    // capitalize first letter:
-    beater = beater.charAt(0).toUpperCase() + beater.slice(1);
-    bested = bested.charAt(0).toUpperCase() + bested.slice(1);
-    const explanation = verdict === "draw" ? `${beater} draws ${bested}` :
-        `${beater} beats ${bested}`;
-
-    console.log(`You ${verdict}! ${explanation}`)
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-console.log(`Human chose ${humanChoice}, computer chose ${computerChoice}`);
-playRound(humanChoice, computerChoice);
+playGame();
